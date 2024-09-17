4030cda4cce24d886344d5a2370dee997844badb
dohun08 <bemik624@gmail.com>
2024-09-17 23:44:56 +0900

    add:인서트추가

--- todoList/src/routers/Main.js
+++ todoList/src/routers/Main.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import '../css/Main.css'
 function Main(){
     const [Todos, setTodos] = useState([])
+    const [newTodo, setNewTodo] = useState(null)
     const getTodos = async ()=>{
         const response = await fetch("http://localhost:8080")
         const json = await response.json()
@@ -10,20 +11,44 @@ function Main(){
     useEffect(() => {
         getTodos();
     }, []);
+    const valueChange = (e)=>{
+        setNewTodo(e.target.value)
+    }
+    const addTodo = async () => {
+        if (newTodo.trim() === "") return;
+        try {
+            const response = await fetch('http://localhost:8080/insert', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ task: newTodo }), // 새로운 할일을 서버로 전송
+            });
+            if (response.ok) {
+                setNewTodo(''); // 입력 필드를 비워줌
+                getTodos(); // 새로운 할일을 추가한 뒤 목록을 다시 불러옴
+                console.log("getTodos")
+            } else {
+                console.error('서버에서 오류 발생');
+            }
+        } catch (error) {
+            console.error('데이터 전송 실패:', error);
+        }
+    };
     console.log(Todos)
     return(
         <div className={"container"}>
             <section className={"headerBox"}>
                 <h1>할일</h1>
-                <form>
-                    <input type={"text"} placeholder={"할일을 입력해주세요"}/>
-                    <input type={"button"} value={"추가하기"}/>
+                <form method={"POST"}>
+                    <input type={"text"} placeholder={"할일을 입력해주세요"} value={newTodo} onChange={valueChange}/>
+                    <input type={"button"} onClick={addTodo} value={"추가하기"}/>
                 </form>
             </section>
             <section className={"todoList"}>
                 {Todos.length > 0 ? Todos.map((element)=> {
                     return (
-                        <div className={"todo"}>
+                        <div className={"todo"} key={element.id}>
                             <p>{element.task}</p>
                             <div className={"buttonBox"}>
                                 <button type={"button"}>완료하기</button>
@@ -32,6 +57,7 @@ function Main(){
                         </div>
                     )
                 }) : null}
+
 
             </section>
         </div>
